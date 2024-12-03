06e2afce569862ab44fb3ba0d6a6e0f302f76c42
Michael <berndt@cybersmart.co.za>
2024-12-03 10:38:12 +0200

    added separate products component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,12 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductsComponent } from './components/products/products.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, ProductsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
-export class AppComponent {
-  products = [
-    {name: "Potatoes", stock: 300, price: 10},
-    {name: "Maize heads", stock: 200, price: 15},
-    {name: "Carrots", stock: 100, price: 7}
-  ];
-}
+export class AppComponent { }

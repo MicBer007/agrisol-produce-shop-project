b840ae08dfb050df3c701e88e099735b5f1e40d2
Michael <berndt@cybersmart.co.za>
2024-12-03 10:31:39 +0200

    created basic shop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'shop-project';
+  products = [
+    {name: "Potatoes", stock: 300, price: 10},
+    {name: "Maize heads", stock: 200, price: 15},
+    {name: "Carrots", stock: 100, price: 7}
+  ];
 }
